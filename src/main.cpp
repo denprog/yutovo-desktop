f6cb9e25a767c13fb4ef1ea9a7dca8c4b6cbb8d8
Denis Gordenin <dgordenin@gmail.com>
2023-01-23 20:00:52 +0300

    Init.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include <yutovo_editor/document.h>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
