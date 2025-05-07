d0a20b456cda67f92f27b212dd839eedf7c260da
Denis Gordenin <dgordenin@gmail.com>
2025-05-07 21:45:36 +0300

    Add a suffix.

--- deb/make_deb.sh
+++ deb/make_deb.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-rm -rf yutovo-desktop_1.0.2-1_amd64/*
-mkdir -p yutovo-desktop_1.0.2-1_amd64/opt/yutovo/
-mkdir -p yutovo-desktop_1.0.2-1_amd64/usr/share/applications/
-mkdir -p yutovo-desktop_1.0.2-1_amd64/usr/share/icons/hicolor/256x256/apps/
-mkdir -p yutovo-desktop_1.0.2-1_amd64/DEBIAN/
+rm -rf yutovo-desktop_1.0.2-1_ubuntu24_amd64/*
+mkdir -p yutovo-desktop_1.0.2-1_ubuntu24_amd64/opt/yutovo/
+mkdir -p yutovo-desktop_1.0.2-1_ubuntu24_amd64/usr/share/applications/
+mkdir -p yutovo-desktop_1.0.2-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/
+mkdir -p yutovo-desktop_1.0.2-1_ubuntu24_amd64/DEBIAN/
 
-cp control yutovo-desktop_1.0.2-1_amd64/DEBIAN/
-cp ../build/RelWithDebugInfo/src/yutovo_desktop yutovo-desktop_1.0.2-1_amd64/opt/yutovo/
-cp -r ../../yutovo_server/library yutovo-desktop_1.0.2-1_amd64/opt/yutovo/
-cp ../build/*.qm yutovo-desktop_1.0.2-1_amd64/opt/yutovo/
-cp ../src/images/mainicon.png yutovo-desktop_1.0.2-1_amd64/usr/share/icons/hicolor/256x256/apps/yutovo.png
+cp control yutovo-desktop_1.0.2-1_ubuntu24_amd64/DEBIAN/
+cp ../build/RelWithDebugInfo/src/yutovo_desktop yutovo-desktop_1.0.2-1_ubuntu24_amd64/opt/yutovo/
+cp -r ../../yutovo_server/library yutovo-desktop_1.0.2-1_ubuntu24_amd64/opt/yutovo/
+cp ../build/*.qm yutovo-desktop_1.0.2-1_ubuntu24_amd64/opt/yutovo/
+cp ../src/images/mainicon.png yutovo-desktop_1.0.2-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/yutovo.png
 
-chmod g-w yutovo-desktop_1.0.2-1_amd64/opt/yutovo/yutovo_desktop
-chmod o-w yutovo-desktop_1.0.2-1_amd64/opt/yutovo/yutovo_desktop
+chmod g-w yutovo-desktop_1.0.2-1_ubuntu24_amd64/opt/yutovo/yutovo_desktop
+chmod o-w yutovo-desktop_1.0.2-1_ubuntu24_amd64/opt/yutovo/yutovo_desktop
 
-cp yutovo.desktop yutovo-desktop_1.0.2-1_amd64/usr/share/applications/
+cp yutovo.desktop yutovo-desktop_1.0.2-1_ubuntu24_amd64/usr/share/applications/
 
-dpkg-deb --build yutovo-desktop_1.0.2-1_amd64
+dpkg-deb --build yutovo-desktop_1.0.2-1_ubuntu24_amd64
