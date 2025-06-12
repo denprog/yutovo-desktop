4699520ac4f43b66cde9cdfe3150cd17dd8c9e1d
dgordenin <dgordenin@gmail.com>
2025-06-12 08:58:01 +0300

    Add Flatpak build.

---
+++ setup/Flatpak/make-yutovo-bin.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -rf yutovo-bin-1.0.3
+mkdir yutovo-bin-1.0.3
+cd yutovo-bin-1.0.3
+cp ../docker_build/build/src/yutovo_desktop .
+cp -r ../../../../yutovo_server/library .
+cp ../../../src/translations/*.qm .
+cp ../yutovo.png ./com.yutovo.yutovo.png
+cp ../yutovo.desktop ./com.yutovo.yutovo.desktop
+cp ../com.yutovo.yutovo.metainfo.xml .
+cp ../run.sh .
+
+cd ..
+tar -czvf yutovo-bin-1.0.3.tar.gz yutovo-bin-1.0.3/*
---
+++ setup/Flatpak/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /app/bin && ./yutovo_desktop --logs-path=$XDG_DATA_HOME/log
