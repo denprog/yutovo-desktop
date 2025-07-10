ccca834c3115fe16e72ce49a518a57d8c0af395a
dgordenin <dgordenin@gmail.com>
2025-07-10 20:58:27 +0300

    Register mime type yut.

--- setup/Ubuntu/make_deb.sh
+++ setup/Ubuntu/make_deb.sh
@@ -4,17 +4,23 @@ rm -rf yutovo-desktop_1.1.1-1_ubuntu24_amd64/*
 mkdir -p yutovo-desktop_1.1.1-1_ubuntu24_amd64/opt/yutovo/
 mkdir -p yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/applications/
 mkdir -p yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/
+mkdir -p yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/mime/packages/
+mkdir -p yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/mimetypes/
 mkdir -p yutovo-desktop_1.1.1-1_ubuntu24_amd64/DEBIAN/
 
 cp control yutovo-desktop_1.1.1-1_ubuntu24_amd64/DEBIAN/
+cp postinst yutovo-desktop_1.1.1-1_ubuntu24_amd64/DEBIAN/
+cp prerm yutovo-desktop_1.1.1-1_ubuntu24_amd64/DEBIAN/
 cp ../../build/RelWithDebugInfo/src/yutovo_desktop yutovo-desktop_1.1.1-1_ubuntu24_amd64/opt/yutovo/
 cp -r ../../../yutovo_server/library yutovo-desktop_1.1.1-1_ubuntu24_amd64/opt/yutovo/
 cp ../../src/translations/*.qm yutovo-desktop_1.1.1-1_ubuntu24_amd64/opt/yutovo/
 cp ../../src/images/mainicon.png yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/yutovo.png
+cp ../../src/images/mainicon.png yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/mimetypes/application-x-yut.png
 
 chmod g-w yutovo-desktop_1.1.1-1_ubuntu24_amd64/opt/yutovo/yutovo_desktop
 chmod o-w yutovo-desktop_1.1.1-1_ubuntu24_amd64/opt/yutovo/yutovo_desktop
 
 cp yutovo.desktop yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/applications/
+cp yut.xml yutovo-desktop_1.1.1-1_ubuntu24_amd64/usr/share/mime/packages/
 
 dpkg-deb --build yutovo-desktop_1.1.1-1_ubuntu24_amd64
