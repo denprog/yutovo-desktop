e6570411c7abbbc4b201dd7dbe47e85de37ccf78
dgordenin <dgordenin@gmail.com>
2025-08-05 23:22:02 +0300

    File associations for Flatpak version.

--- setup/Flatpak/make-yutovo-bin.sh
+++ setup/Flatpak/make-yutovo-bin.sh
@@ -6,10 +6,10 @@ cd yutovo-bin-1.1.3
 cp ../docker_build/build/src/yutovo_desktop .
 cp -r ../../../../yutovo_server/library .
 cp ../../../src/translations/*.qm .
-cp ../yutovo.png ./com.yutovo.yutovo.png
-cp ../yutovo.desktop ./com.yutovo.yutovo.desktop
+cp ../yutovo.png .
+cp ../yutovo.desktop .
 cp ../com.yutovo.yutovo.metainfo.xml .
-cp ../com.yutovo.yutovo.xml .
+cp ../yutovo.xml .
 cp ../index.theme .
 cp ../run.sh .
 
--- setup/Flatpak/run.sh
+++ setup/Flatpak/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cd /app/bin && ./yutovo_desktop --logs-path=$XDG_DATA_HOME/log
+file="$1"
+
+cd /app/bin || exit 1
+
+if [[ -n "$file" ]]; then
+    ./yutovo_desktop "$file" --logs-path="$XDG_DATA_HOME/log"
+else
+    ./yutovo_desktop --logs-path="$XDG_DATA_HOME/log"
+fi
