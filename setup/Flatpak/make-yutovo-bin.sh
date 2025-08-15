4d40ca8bfae7307b552beacb0f3ed0a7005fdf2c
dgordenin <dgordenin@gmail.com>
2025-08-15 21:28:15 +0300

    Fix version.

--- setup/Flatpak/make-yutovo-bin.sh
+++ setup/Flatpak/make-yutovo-bin.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm -rf yutovo-bin-1.1.3
-mkdir yutovo-bin-1.1.3
-cd yutovo-bin-1.1.3
+rm -rf yutovo-bin-1.2.1
+mkdir yutovo-bin-1.2.1
+cd yutovo-bin-1.2.1
 cp ../docker_build/build/src/yutovo_desktop .
 cp -r ../../../../yutovo_server/library .
 cp ../../../src/translations/*.qm .
@@ -14,4 +14,4 @@ cp ../index.theme .
 cp ../run.sh .
 
 cd ..
-tar -czvf yutovo-bin-1.1.3.tar.gz yutovo-bin-1.1.3/*
+tar -czvf yutovo-bin-1.2.1.tar.gz yutovo-bin-1.2.1/*
