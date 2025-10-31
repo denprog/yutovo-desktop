2f63371f5e46bbe87b3bfba525ca9f24a8104737
dgordenin <dgordenin@gmail.com>
2025-10-31 08:42:25 +0300

    Update deb version.

--- setup/Ubuntu/make_deb.sh
+++ setup/Ubuntu/make_deb.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-rm -rf yutovo-desktop_1.3.1-1_ubuntu24_amd64/*
-mkdir -p yutovo-desktop_1.3.1-1_ubuntu24_amd64/opt/yutovo/
-mkdir -p yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/applications/
-mkdir -p yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/
-mkdir -p yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/mime/packages/
-mkdir -p yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/mimetypes/
-mkdir -p yutovo-desktop_1.3.1-1_ubuntu24_amd64/DEBIAN/
+rm -rf yutovo-desktop_1.3.2-1_ubuntu24_amd64/*
+mkdir -p yutovo-desktop_1.3.2-1_ubuntu24_amd64/opt/yutovo/
+mkdir -p yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/applications/
+mkdir -p yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/
+mkdir -p yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/mime/packages/
+mkdir -p yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/mimetypes/
+mkdir -p yutovo-desktop_1.3.2-1_ubuntu24_amd64/DEBIAN/
 
-cp control yutovo-desktop_1.3.1-1_ubuntu24_amd64/DEBIAN/
-cp postinst yutovo-desktop_1.3.1-1_ubuntu24_amd64/DEBIAN/
-cp prerm yutovo-desktop_1.3.1-1_ubuntu24_amd64/DEBIAN/
-cp ../../build/release/src/yutovo-desktop yutovo-desktop_1.3.1-1_ubuntu24_amd64/opt/yutovo/
-cp -r ../../../yutovo-server/library yutovo-desktop_1.3.1-1_ubuntu24_amd64/opt/yutovo/
-cp ../../build/release/src/*.qm yutovo-desktop_1.3.1-1_ubuntu24_amd64/opt/yutovo/
-cp ../../src/images/mainicon.png yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/yutovo.png
-cp ../../src/images/mainicon.png yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/mimetypes/application-x-yut.png
+cp control yutovo-desktop_1.3.2-1_ubuntu24_amd64/DEBIAN/
+cp postinst yutovo-desktop_1.3.2-1_ubuntu24_amd64/DEBIAN/
+cp prerm yutovo-desktop_1.3.2-1_ubuntu24_amd64/DEBIAN/
+cp ../../build/release/src/yutovo-desktop yutovo-desktop_1.3.2-1_ubuntu24_amd64/opt/yutovo/
+cp -r ../../../yutovo-server/library yutovo-desktop_1.3.2-1_ubuntu24_amd64/opt/yutovo/
+cp ../../build/release/src/*.qm yutovo-desktop_1.3.2-1_ubuntu24_amd64/opt/yutovo/
+cp ../../src/images/mainicon.png yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/apps/yutovo.png
+cp ../../src/images/mainicon.png yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/icons/hicolor/256x256/mimetypes/application-x-yut.png
 
-chmod g-w yutovo-desktop_1.3.1-1_ubuntu24_amd64/opt/yutovo/yutovo-desktop
-chmod o-w yutovo-desktop_1.3.1-1_ubuntu24_amd64/opt/yutovo/yutovo-desktop
+chmod g-w yutovo-desktop_1.3.2-1_ubuntu24_amd64/opt/yutovo/yutovo-desktop
+chmod o-w yutovo-desktop_1.3.2-1_ubuntu24_amd64/opt/yutovo/yutovo-desktop
 
-cp yutovo.desktop yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/applications/
-cp yut.xml yutovo-desktop_1.3.1-1_ubuntu24_amd64/usr/share/mime/packages/
+cp yutovo.desktop yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/applications/
+cp yut.xml yutovo-desktop_1.3.2-1_ubuntu24_amd64/usr/share/mime/packages/
 
-dpkg-deb --build yutovo-desktop_1.3.1-1_ubuntu24_amd64
+dpkg-deb --build yutovo-desktop_1.3.2-1_ubuntu24_amd64
