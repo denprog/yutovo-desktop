59292813ee499e7e8edd89966f9c9ba29dcbe182
dgordenin <dgordenin@gmail.com>
2025-12-18 20:39:16 +0300

    Update for AUR.

--- setup/Ubuntu/make_deb.sh
+++ setup/Ubuntu/make_deb.sh
@@ -21,6 +21,6 @@ chmod g-w yutovo-desktop_1.4.1-1_ubuntu24_amd64/opt/yutovo/yutovo-desktop
 chmod o-w yutovo-desktop_1.4.1-1_ubuntu24_amd64/opt/yutovo/yutovo-desktop
 
 cp yutovo.desktop yutovo-desktop_1.4.1-1_ubuntu24_amd64/usr/share/applications/
-cp yut.xml yutovo-desktop_1.4.1-1_ubuntu24_amd64/usr/share/mime/packages/
+cp ../yut.xml yutovo-desktop_1.4.1-1_ubuntu24_amd64/usr/share/mime/packages/
 
 dpkg-deb --build yutovo-desktop_1.4.1-1_ubuntu24_amd64
