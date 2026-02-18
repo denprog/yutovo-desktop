83bc12812878551fb7db8bd5eb170ac2a64e13d8
dgordenin <dgordenin@gmail.com>
2026-02-18 21:17:08 +0300

    Change a hotkey.

--- src/command_map.cpp
+++ src/command_map.cpp
@@ -83,7 +83,7 @@ void ShortcutsMap::Init(DocumentPtr _document, QWidget* document_widget)
     Add(QKeySequence("/"), "\\div", std::function<void ()>(std::bind(&Document::InsertDivision, document.get(), true, false)), CommandContext::Formula);
     Add(QKeySequence("Ctrl+Shift+P"), "\\pow", std::function<void()>(std::bind(&Document::InsertPower, document.get(), true, false)), CommandContext::Formula);
     Add(QKeySequence("Ctrl+Shift+S"), "\\sub", std::function<void ()>(std::bind(&Document::InsertSubscript, document.get(), true, false)), CommandContext::Formula);
-    Add(QKeySequence("Ctrl+Shift+N"), "\\nth", std::function<void ()>(std::bind(&Document::InsertNthRoot, document.get(), true, false)));
+    Add(QKeySequence("Ctrl+Shift+R"), "\\nth", std::function<void ()>(std::bind(&Document::InsertNthRoot, document.get(), true, false)));
     Add(QKeySequence("Ctrl+Shift+Q"), "\\sqrt", std::function<void ()>(std::bind(&Document::InsertSquareRoot, document.get(), true, false)));
     Add(QKeySequence(""), '=', "\\equal", std::function<void ()>(std::bind(&Document::InsertEquation, document.get(), ResultType::AUTO, true)), 
         CommandContext::Formula);
