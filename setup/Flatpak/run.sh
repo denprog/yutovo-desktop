15aac5f38cccda0bd72b1e1b70624cbf16870c63
dgordenin <dgordenin@gmail.com>
2026-06-26 21:13:53 +0300

    Fix Flatpak build.

--- setup/Flatpak/run.sh
+++ setup/Flatpak/run.sh
@@ -5,7 +5,7 @@ file="$1"
 cd /app/bin || exit 1
 
 if [[ -n "$file" ]]; then
-    LD_LIBRARY_PATH=/app/bin:$LD_LIBRARY_PATH ./yutovo-desktop "$file" --logs-path="$XDG_DATA_HOME/log"
+    LD_LIBRARY_PATH=/app/lib:/app/bin:$LD_LIBRARY_PATH ./yutovo-desktop "$file" --logs-path="$XDG_DATA_HOME/log"
 else
-    LD_LIBRARY_PATH=/app/bin:$LD_LIBRARY_PATH ./yutovo-desktop --logs-path="$XDG_DATA_HOME/log"
+    LD_LIBRARY_PATH=/app/lib:/app/bin:$LD_LIBRARY_PATH ./yutovo-desktop --logs-path="$XDG_DATA_HOME/log"
 fi
